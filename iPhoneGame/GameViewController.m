111675f18f8ba8d58bab28fdc2ed1cb8322e6432
Dylan Owen <dowen@ucdavis.edu>
2012-02-18 10:53:19 -0800

    Just testing out open gl

--- iPhoneGame/GameViewController.m
+++ iPhoneGame/GameViewController.m
@@ -14,6 +14,8 @@
 {
 	GLuint _dirtVertexBuffer;
 	GLuint _dirtIndexBuffer;
+	
+	float _back;
 }
 
 @property (nonatomic, strong) GameModel *mainGame;
@@ -56,9 +58,26 @@
 }
 */
 
+- (void)setupGL
+{
+	[EAGLContext setCurrentContext:self.context];
+	self.effect = [[GLKBaseEffect alloc] init];
+	
+	glGenBuffers(1, &_dirtVertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, _dirtVertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(self.mainGame->dirt), self.mainGame->dirt, GL_DYNAMIC_DRAW);
+	
+	glGenBuffers(1, &_dirtIndexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _dirtIndexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(self.mainGame->dirtIndices), self.mainGame->dirtIndices, GL_STATIC_DRAW);
+
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+	 
+	 _back = -30.0f;
 	 
 	 self.context = [[EAGLContext alloc] initWithAPI: kEAGLRenderingAPIOpenGLES2];
 	 
@@ -71,21 +90,8 @@
 	 view.context = self.context;
 	 
 	 self.mainGame = [[GameModel alloc] init];
-}
-
-- (void)setupGL
-{
-	[EAGLContext setCurrentContext:self.context];
-	self.effect = [[GLKBaseEffect alloc] init];
-	
-	glGenBuffers(1, &_dirtVertexBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, _dirtVertexBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(self.mainGame->dirt), self.mainGame->dirt, GL_DYNAMIC_DRAW);
-	
-	glGenBuffers(1, &_indexBuffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _dirtIndexBuffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(self.mainGame->dirtIndices), self.mainGame->dirtIndices, GL_STATIC_DRAW);
-
+	 
+	 [self setupGL];
 }
 
 - (void)tearDownGL
@@ -116,7 +122,46 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);	
+}
+
+#pragma mark - GLKViewDelegate
+
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
+{
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	[self.effect prepareToDraw];
+	
+	glBindBuffer(GL_ARRAY_BUFFER, _dirtVertexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _dirtIndexBuffer);
+	
+	glEnableVertexAttribArray(GLKVertexAttribPosition);
+	glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(DirtVertex), (const GLvoid *) offsetof(DirtVertex, position)); 
+	glEnableVertexAttribArray(GLKVertexAttribColor);
+	glVertexAttribPointer(GLKVertexAttribColor, 4, GL_FLOAT, GL_FALSE, sizeof(DirtVertex), (const GLvoid *) offsetof(DirtVertex, color));
+	
+	glDrawElements(GL_POINTS, sizeof(self.mainGame->dirtIndices)/sizeof(self.mainGame->dirtIndices[0]), GL_UNSIGNED_BYTE, 0);
+}
+
+#pragma mark - GLKViewControllerDelegate
+
+- (void)update
+{
+	float aspect = fabsf(self.view.bounds.size.width / self.view.bounds.size.height);
+	GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0f), aspect, 10.0f, 100.0f);
+	self.effect.transform.projectionMatrix = projectionMatrix;
+	
+	_back -= 1.0 * self.timeSinceLastUpdate;
+	
+	self.effect.transform.modelviewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, _back);
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	NSLog(@"back: %f", _back);
+	//self.paused = !self.paused;
 }
 
 @end
